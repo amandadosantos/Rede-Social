71a302b20ed79d1a56e273cfb2c477f652909ad3
amandadosantos <amanda.dos.santos04@escola.pr.gov.br>
2026-08-20 08:49:34 -0300

    ok

--- script.js
+++ script.js
@@ -4,7 +4,33 @@ document.addEventListener("DOMContentLoaded", () =>{
     if (!likeBtn) return; 
      const likeSvg = likeBnt.querySelector("svg"); 
 
+
     //localiza o contador
 
-    let textNode =  Array
+    let textNode =  Array.from(likeBnt.childNodes).find(node) => node.nodeType
+    === Node.TEXT_NODE && node.textContent.trim() !==""
+);
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+    textNode.textContent = `0`;
+}
+
+//coração
+function applyLikedStyle (){
+likeSvg.style.fill = "#ef4444";
+likeSvg.tyle.stroke = "#ef4444";
+likeSvg.style.color = "#ef4444";
+ 
+
+ //efeito curtida
+ likeSvg.style.transform = "scale(1.3)";
+ setTimeout(() => (likeSvg.style.transform = "scale(1)")150);
+}
+
+
+
 })
